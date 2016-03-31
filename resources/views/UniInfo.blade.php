f3bd45a256101ee18444ccd2ea871c052c213fa3
MianAshar <ashar617@gmail.com>
2016-03-31 12:00:36 +0500

    blade files added

---
+++ resources/views/UniInfo.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Add university information</title>
+
+    <link href="{{ asset('style/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('style/css/ambassadorstyling.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('style/js/bootstrap.min.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('style/css/jquery.min.css') }}" rel="stylesheet" type="text/css" >
+</head>
+
+<body>
+<header>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <div class="logo">
+            </div>
+    	</div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+			<div class="top-right">		        
+                <a href="#">LOGOUT</a>
+             </div>
+		</div>
+	</div>
+</div>
+</header>
+
+<main>
+    <div class="container">
+    	<div class="row">
+        
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+	            <div class="block-uni">
+                	<h3 class="h3-1-uni-block">ABOUT</h3>
+                    <a href="#">
+                    <img src="{{ asset('style/images/adduni/brown.png') }}" class="img-responsive" />
+                    </a>
+            	</div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="block-uni">
+                	<h3 class="h3-2-uni-block">ADD UNIVERSITY</h3>
+                    <a href="#">
+                    <img src="{{ asset('style/images/adduni/green.png') }}" class="img-responsive" />
+                    </a>
+            	</div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="block-uni">
+                	<h3 class="h3-3-uni-block">CONTACT</h3>
+                    <a href="#">
+                    <img src="{{ asset('style/images/adduni/red.png') }}" class="img-responsive" />
+                    </a>
+            	</div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="block-uni">
+                	<h3 class="h3-4-uni-block">MALE/FEMALE</h3>
+                    <a href="#">
+                    <img src="{{ asset('style/images/adduni/blue.png') }}" class="img-responsive" />
+                    </a>
+            	</div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="block-uni">
+                	<h3 class="h3-5-uni-block">ADD LOCATION</h3>
+                    <a href="#">
+                    <img src="{{ asset('style/images/adduni/yellow.png') }}" class="img-responsive" />
+                    </a>
+            	</div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="block-uni">
+                	<h3 class="h3-6-uni-block">ADD PHOTOS</h3>
+                    <a href="#">
+                    <img src="{{ asset('style/images/adduni/gray.png') }}" class="img-responsive" />
+                    </a>
+            	</div>
+            </div>
+            
+         </div>
+    </div>
+</main>
+</body>
+</html>
---
+++ resources/views/adduniversity.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Add University</title>
+
+    <link href="{{ asset('style/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('style/css/ambassadorstyling.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('style/js/bootstrap.min.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('style/css/jquery.min.css') }}" rel="stylesheet" type="text/css" >
+
+</head>
+
+<body>
+<header>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <div class="logo">
+            </div>
+    	</div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+			<div class="top-right">		        
+                <a href="#">SETTINGS</a>
+                <a href="#">LOGOUT</a>
+             </div>
+		</div>
+</div>
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        	<a href="#">
+                <div class="add-university">
+                ADD UNIVERSITY
+                </div>
+          	</a>
+        </div>
+    </div>
+</div>
+</header>
+<main>
+    <div class="container">
+    	<div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+	            <div class="block-uni">
+                	<h3 class="h3-1-uni-block">ADD UNIVERSITY</h3>
+                    <a href="#">
+                    <img src="{{ asset('style/images/adduni/brown.png') }}" class="img-responsive" />
+                    </a>
+            	</div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="block-uni">
+                	<h3 class="h3-2-uni-block">ADD UNIVERSITY</h3>
+                    <a href="#">
+                    <img src="{{ asset('style/images/adduni/green.png') }}" class="img-responsive" />
+
+                    </a>
+            	</div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="block-uni">
+                	<h3 class="h3-3-uni-block">ADD UNIVERSITY</h3>
+                    <a href="#">
+                    <img src="{{ asset('style/images/adduni/red.png') }}" class="img-responsive" />
+                    </a>
+            	</div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="block-uni">
+                	<h3 class="h3-4-uni-block">ADD UNIVERSITY</h3>
+                    <a href="#">
+                    <img src="{{ asset('style/images/adduni/blue.png') }}" class="img-responsive" />
+                    </a>
+            	</div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="block-uni">
+                	<h3 class="h3-5-uni-block">ADD UNIVERSITY</h3>
+                    <a href="#">
+                    <img src="{{ asset('style/images/adduni/yellow.png') }}" class="img-responsive" />
+                    </a>
+            	</div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="block-uni">
+                	<h3 class="h3-6-uni-block">ADD UNIVERSITY</h3>
+                    <a href="#">
+                    <img src="{{ asset('style/images/adduni/gray.png') }}" class="img-responsive" />
+                    </a>
+            	</div>
+            </div>
+            
+         </div>
+    </div>
+</main>
+
+</body>
+</html>
